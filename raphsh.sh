3103c96d30ec12115f05c3465ab44ab232070939
Raphaël Casimir <raphael.casimir@sensefly.com>
2019-10-09 18:32:37 +0200

    Added Linux subsystem for Windows support

--- raphsh.sh
+++ raphsh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ## raphsh.sh v2.3 by Raphaël Casimir, 14.01.2019
 ## This script is a shell auto-configurator for ubuntu-like distros
+## It also works with Linux Subsystem for Windows
 ## Do no use sudo
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -29,6 +30,16 @@ cd ~
 gawk -i inplace -f $DIR/zshrc.awk ~/.zshrc
 
 echo "source ~/.zshrc" | zsh
+
+function winDetect() {
+    echo $(cat /proc/version | grep -i microsoft)
+}
+
+if [ "$(winDetect)" != "" ]
+then
+    printf "\n# Launch Zsh \nif [ -t 1 ]; then \nexec zsh \nfi" >> .bashrc
+    chmod -R -w /home/$USER/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
 
 echo "raphsh setup done, please log out and back in if changes are not applied"
 
